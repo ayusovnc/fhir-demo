8debcbf7e78f592bccf72d5b4b3b1425ce0ebce6
Andrei Yusov <ayusov@navigatingcancer.com>
2021-05-20 12:16:22 -0700

    saving perf testing scripts

---
+++ simple_spring_boot/perf_test/get_1000_random_patients.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+gzip -dc pids.100k.gz | sed '1d' | shuf -n 1000 | while read n; do
+    tm=`curl -s -w '%{time_connect}\t%{time_starttransfer}\t%{time_total}\n' -o /dev/zero http://localhost:8080/fhir/Patient/${n}`
+    echo ${n} ${tm}
+done
