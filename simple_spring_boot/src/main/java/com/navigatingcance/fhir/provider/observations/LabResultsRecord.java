18ca122a249ea9b8b294befc6444deb2beb697c5
Andrei Yusov <ayusov@navigatingcancer.com>
2021-05-21 10:27:19 -0700

    adding few more observation fields; fix category duplication

--- simple_spring_boot/src/main/java/com/navigatingcance/fhir/provider/observations/LabResultsRecord.java
+++ simple_spring_boot/src/main/java/com/navigatingcance/fhir/provider/observations/LabResultsRecord.java
@@ -2,6 +2,7 @@ package com.navigatingcance.fhir.provider.observations;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
@@ -10,6 +11,7 @@ import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Observation.ObservationStatus;
 import org.hl7.fhir.r4.model.Quantity;
 import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.StringType;
 import org.hl7.fhir.r4.model.codesystems.ObservationCategory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,14 +49,32 @@ public record LabResultsRecord(Integer id, Integer clinic_id, Integer person_id,
         } catch(Exception ex) {
             log.error("invalid quantity value {} in clinic_test_results {}", quantity, id); 
         }
-        CodeableConcept group = res.addCategory().setText(group_identifier);
+        res.addCategory().setText(group_identifier);
         ObservationCategory obsCat = groupNameToObsCategory(group_identifier);
         if( obsCat != ObservationCategory.NULL) {
             Coding groupCoding = res.addCategory().setText(group_identifier).addCoding();
             groupCoding.setSystem(obsCat.getSystem());
             groupCoding.setCode(obsCat.toCode());
-            group.getCoding().add(groupCoding);
         }
+
+        // reference range https://www.hl7.org/fhir/observation-definitions.html#Observation.referenceRange
+        if (normal_range_min != null) {
+            res.addReferenceRange().setLow(new Quantity(normal_range_min));
+        }
+        if (normal_range_max != null) {
+            res.addReferenceRange().setHigh(new Quantity(normal_range_max));
+        }
+
+        // interpretation https://www.hl7.org/fhir/observation-definitions.html#Observation.interpretation
+        CodeableConcept interpr = new CodeableConcept();
+        interpr.setText(interpretation_concept);
+        res.addInterpretation(interpr);
+        // TODO. Add coding https://www.hl7.org/fhir/valueset-observation-category.html
+
+        // Component https://www.hl7.org/fhir/observation-definitions.html#Observation.component
+        res.addComponent().setValue(new StringType(component_name) );
+        // TODO. Add component coding 
+
         return res;
     }
 
